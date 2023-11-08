786f3f318ec68717fd569e1c798d4d79c47bd85a
rcollettatdirigible <rcollett@dirigible.systems>
2023-11-08 12:42:54 -0500

    Finished the todo's

--- index.js
+++ index.js
@@ -1,13 +1,87 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require('inquirer');
+const fs = require('fs'); //fs is needed to create a file
+
+/*
+WHEN I enter my project title
+WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+WHEN I choose a license for my application from a list of options
+
+THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
+WHEN I enter my GitHub username
+THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+WHEN I enter my email address
+THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+WHEN I click on the links in the Table of Contents
+*/
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "title",
+        message: "What is the title of your project?",
+        type: "input"
+    },
+    {
+        name: "description",
+        message: "Describe your project",
+        type: "input"
+    },
+    {
+        name: "installation",
+        message: "What are the installation instructions?",
+        type: "input"
+    },
+    {
+        name: "usage",
+        message: "What is the usage information?",
+        type: "input"
+    },
+    {
+        name: "contribution",
+        message: "What are the contribution guidelines?",
+        type: "input"
+    },
+    {
+        name: "tests",
+        message: "What are the test instructions?",
+        type: "input"
+    },
+    {
+        name: "license",
+        message: "Choose a license from this list:",
+        type: "list",
+        choices:["Eclipse Public License 2.0", "MIT License", "Apache 2.0 License", "No License"]
+    },
+    {
+        name: "username",
+        message: "What is your GitHub username?",
+        type: "input"
+    },
+    {
+        name: "email",
+        message: "What is your email?",
+        type: "input",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), err =>{
+        if(err) throw err
+        console.log("success")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(data => {
+        writeToFile("./output/README.md", data)
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,113 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT License") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  }
+  else if (license === "No License") {
+    return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+  }
+  else if (license === "Apache 2.0 License") {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+  else {
+    return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
+  }
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT License") {
+    return `https://opensource.org/license/mit/`
+  }
+  else if (license === "No License") {
+    return " "
+  }
+  else if (license === "Apache 2.0 License") {
+    return `https://opensource.org/license/apache-2-0/`
+  }
+  else {
+    return `https://opensource.org/license/epl-1-0/`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "MIT License") {
+    return `THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+  }
+  else if (license === "No License") {
+    return " "
+  }
+  else if (license === "Apache 2.0 License") {
+    return `Unless required by applicable law or agreed to in writing, Licensor provides the Work (and each Contributor provides its Contributions) on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied, including, without limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE. You are solely responsible for determining the appropriateness of using or redistributing the Work and assume any risks associated with Your exercise of permissions under this License.`
+  }
+  else {
+    return `EXCEPT AS EXPRESSLY SET FORTH IN THIS AGREEMENT, NEITHER RECIPIENT NOR ANY CONTRIBUTORS SHALL HAVE ANY LIABILITY FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING WITHOUT LIMITATION LOST PROFITS), HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OR DISTRIBUTION OF THE PROGRAM OR THE EXERCISE OF ANY RIGHTS GRANTED HEREUNDER, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data)}
+
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents (Optional)
+  
+  If your README is long, add a table of contents to make it easy for users to find what they need.
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+  
+  ## Installation
+  
+  ${data.installation}
+
+  ## Usage
+  
+  ${data.usage}
+
+  ## License
+  
+  ${renderLicenseSection(data)}  
+  ---
+  
+  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+  
+  ## Badges
+  
+  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+  
+  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  
+  ## Features
+  
+  If your project has a lot of features, list them here.
+  
+  ## Contribution
+  
+${data.contribution}
+  
+  ## Tests  
+
+  ${data.test}
+
+  ## Questions
+
+  <ul>
+  <li> <a href="https://github.com/${data.username}">GitHub Profile</a> </li>
+  <li> <a href="mailto:${data.email}>Email</a> </li>
+  </ul>
 
 `;
 }
